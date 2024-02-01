d6acc786451cb10045bb9fdf0dfd9e03b909935c
ihnatsen <142001503+ihnatsen@users.noreply.github.com>
2024-02-02 00:05:57 +0100

    Update DisplaySUM.java

--- consl/src/main/java/wit/edu/pl/DisplaySUM.java
+++ consl/src/main/java/wit/edu/pl/DisplaySUM.java
@@ -10,27 +10,28 @@ public class DisplaySUM implements Display, Observer {
 
     @Override
     public void display() {
-
-        Iterator<Object> kilometreIT  =  CRUDUtiels.writeSelect("SELECT SUM(distance) FROM RUNNINGPACE");
-        String sumKilometres  = kilometreIT.next().toString();
-
-
-        Iterator<Object> caloriesSumIT  =  CRUDUtiels.writeSelect("SELECT SUM(calories) FROM RUNNINGPACE");
-        String caloriesSum  = (String) caloriesSumIT.next().toString();
-
-        Iterator<Object> minPaceIT  =  CRUDUtiels.writeSelect("SELECT min(PACE) FROM RUNNINGPACE");
-        String minPace  = (String) minPaceIT.next().toString();
-
-        Sum sum = new Sum(sumKilometres,caloriesSum,minPace);
-        ObjectMapper objectMapper = new ObjectMapper();
-
         try {
-            objectMapper.writeValue(new File(PathToDir.getPathToResourcesFile("consl","sum.json")),sum);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("Export completed");
 
+            Iterator<Object> kilometreIT  =  CRUDUtiels.writeSelect("SELECT SUM(distance) FROM RUNNINGPACE");
+            String sumKilometres  = kilometreIT.next().toString();
+    
+    
+            Iterator<Object> caloriesSumIT  =  CRUDUtiels.writeSelect("SELECT SUM(calories) FROM RUNNINGPACE");
+            String caloriesSum  = (String) caloriesSumIT.next().toString();
+    
+            Iterator<Object> minPaceIT  =  CRUDUtiels.writeSelect("SELECT min(PACE) FROM RUNNINGPACE");
+            String minPace  = (String) minPaceIT.next().toString();
+    
+            Sum sum = new Sum(sumKilometres,caloriesSum,minPace);
+            ObjectMapper objectMapper = new ObjectMapper();
+    
+            try {
+                objectMapper.writeValue(new File(PathToDir.getPathToResourcesFile("consl","sum.json")),sum);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Export completed");
+     }catch (NullPointerException ex) {System.out.println("Don't have data.");}
     }
 
     @Override
